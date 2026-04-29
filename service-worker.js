e4ac83f54005a234af46cb345f9b7119ef450f57
satviksingh837-debug <satviksingh837@gmail.com>
2026-04-29 22:33:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,40 @@
+const CACHE_NAME = "yashomool-v1";
+
+const urlsToCache = [
+  "/yashomoolacademy/",
+  "/yashomoolacademy/index.html",
+  "/yashomoolacademy/manifest.json",
+  "/yashomoolacademy/icon-192.png",
+  "/yashomoolacademy/icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
-  console.log("Service Worker Installed");
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Activate
 self.addEventListener("activate", event => {
-  console.log("Service Worker Activated");
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
